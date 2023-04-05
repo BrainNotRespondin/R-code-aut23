276e8468dd3dbc98b8698b8226b4688da450e8f6
BrainNotRespondin <66655892+BrainNotRespondin@users.noreply.github.com>
2023-04-05 23:12:56 +1000

    Week 2 questions

--- Week 2/1. Sequences.R
+++ Week 2/1. Sequences.R
@@ -3,6 +3,10 @@
 seq(from=1,to=-1,by=2)#q2-6
 2^(0:8)
 
+c(
+  2^seq(from = 1, to = 256, by = 1),
+  2^(0:8)
+)
 c( #q8
   1:10,
   seq(from=12,to=30,by=2)
--- Week 2/2. Plotting math functions.R
+++ Week 2/2. Plotting math functions.R
@@ -9,3 +9,30 @@ plot(x,y,type ="l",
      )
 
 curve(1/(1+exp(-x/5)), from=-30, to=30)
+
+#setup variable for questions
+x<- -50:50
+
+#q1
+y <- 3*x + 9
+
+#q2
+y <- (2*x^2)+(4*x) +1
+
+#q3
+plot((-x^3)+(4*x^2)+(10*x)-7)
+
+#q4
+plot(log(x))
+
+#q5
+plot(sin(x))
+
+#q6
+plot(cos(x/10))
+
+#q7
+plot(sin(x/2)/x)
+
+#q8
+plot(1/(1+exp(-x/5)))
